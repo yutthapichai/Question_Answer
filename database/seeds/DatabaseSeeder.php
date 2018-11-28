22f55ddd85c55d027e5cffc4c77bb470930e87f6
cancelgoes <cancel_go@hotmail.com>
2018-11-28 20:00:32 +0700

    Implement many to many polimorhpic and relationship and make the vote questions controls working

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersQuestionAnswersTableSeeder::class,
             FavoritesTableSeeder::class,
+            VotablesTableSeeder::class,
         ]);
     }
 }
